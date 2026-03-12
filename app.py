bce770932d2bbe43f8e6337ae401063818213079
Martyn Eggleton <martyn.eggleton@gmail.com>
2026-03-12 07:48:50 +0000

    Update title and subheader for key fob application; add download button for STL file

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
+import uuid
+
 import streamlit as st
 from streamlit_stl import stl_from_file, stl_from_text
 from solid2 import *
-#from solid2.extensions.bosl2 import squircle
+import tempfile
+from pathlib import Path
+
+
+if 'stl_file' not in st.session_state:
+    tmp_file_path = str(Path(tempfile.gettempdir()) / str(uuid.uuid4()))+".stl" 
+    st.session_state.stl_file = tmp_file_path
 
 
 st.set_page_config(layout="wide")
 
-st.title("Streamlit STL Examples")
+st.title("Make a Key Fob")
+
 
 cols = st.columns(4)
 with cols[0]:
-    name = st.text_input("What is your Name?", "Streamlit")
+    name = st.text_input("What is your Name?", "Streamlit", key='name')
 with cols[1]:
     length = st.slider("Length", 10, 100,
-                       value=int(round(len(name) * 7.5) + 7.5))
+                       value=int(round(len(name) * 7.5) + 7.5), key='length')
 with cols[2]:
-    depth = st.slider("Depth", 10, 100, value=12)
+    depth = st.slider("Depth", 10, 100, value=12, key='depth')
 
 with cols[3]:
-    height = st.slider("Height", 0.5, 10.0, value=1.0)
+    height = st.slider("Height", 0.5, 10.0, value=1.0, key='height')
+
+
+
+
+# svg is usually just donee by import("file.svg") 
+# from solid2 import import_scad
+# svgshape = import_scad("file.svg")  # Not sure this will work
+# should different designs be done by different classes? 
+# Then use pydantic / dataclasses to generate the UI? 
+## https://github.com/lukasmasuch/streamlit-pydantic
 
 distance_from_corner = 3
+
 
 shape = cube(length, depth, height)
 
@@ -32,55 +52,41 @@ shape -= text(text=name).linear_extrude(height
 shape -= cylinder(h=height*3, r=1).translate(distance_from_corner, depth-distance_from_corner, 0-height)
 
 try:
-    shape.save_as_stl()
+    shape.save_as_stl(st.session_state.stl_file)
 
-    file_path='squirrel.stl'
-
-    with open("app.stl", "r") as file:
-        file_path='app.stl'
         
-    st.subheader("STL viewer")
+    st.subheader("View controls!")
     cols = st.columns(5)
     with cols[0]:
-        color = st.color_picker("Pick a color", "#FF9900", key='color_file')
+        color = st.color_picker("Pick a color", "#005EFF", key='color_file')
     with cols[1]:
         material = st.selectbox("Select a material", ["material", "flat", "wireframe"], key='material_file')
     with cols[2]:
         st.write('\n'); st.write('\n')
-        auto_rotate = st.toggle("Auto rotation", key='auto_rotate_file')
+        auto_rotate = st.toggle("Auto rotation", key='auto_rotate_file',value=True)
     with cols[3]:
         opacity = st.slider("Opacity", min_value=0.0, max_value=1.0, value=1.0, key='opacity_file')
     with cols[4]:
         height = st.slider("Height", min_value=50, max_value=1000, value=500, key='height_file')
 
-    # camera position
-    cols = st.columns(4)
-    with cols[0]:
-        cam_v_angle = st.number_input("Camera Vertical Angle", value=60, key='cam_v_angle')
-    with cols[1]:
-        cam_h_angle = st.number_input("Camera Horizontal Angle", value=-90, key='cam_h_angle')
-    with cols[2]:
-        cam_distance = st.number_input("Camera Distance", value=0, key='cam_distance')
-    with cols[3]:
-        max_view_distance = st.number_input("Max view distance", min_value=1, value=1000, key='max_view_distance')
-
-    stl_from_file(  file_path=file_path, 
+    stl_from_file(  file_path=st.session_state.stl_file, 
                     color=color,
                     material=material,
                     auto_rotate=auto_rotate,
                     opacity=opacity,
                     height=height,
                     shininess=100,
-                    cam_v_angle=cam_v_angle,
-                    cam_h_angle=cam_h_angle,
-                    cam_distance=cam_distance,
-                    max_view_distance=max_view_distance,
+                    cam_v_angle=60,
+                    cam_h_angle=-90,
+                    cam_distance=50,
+                    max_view_distance=1000,
                     key='example1')
 
-    st.download_button("Download STL", data=open("app.stl", "rb"), file_name="app.stl", mime="application/octet-stream")
+    st.download_button("Download STL", data=open(st.session_state.stl_file, "rb").read(), file_name=f"key_fob_{name}.stl", mime="application/octet-stream")
 
 except Exception as e:
     st.error(f"Error: {e}")
-    shape.save_as_scad("app.scad")
-    with open("app.scad", "r") as file:
+    scad_filename = st.session_state.stl_file.replace(".stl", ".scad")
+    shape.save_as_scad(scad_filename)
+    with open(scad_filename, "r") as file:
         st.code(file.read(), language='scad')
