de3e685b987725f9679e0990e6de54322bc1ba00
Martyn Eggleton <martyn.eggleton@gmail.com>
2026-04-21 13:45:04 +0100

    Refactor prototype.py to include center_shape_on_origin and adjust cylinder radius in shape building; remove nicegui from requirements.txt

--- prototype.py
+++ prototype.py
@@ -2,7 +2,7 @@ from solid2 import *
 from pathlib import Path
 from solid2.extensions.bosl2 import squircle
 from designs import KeyFobParams
-from geometry import calculate_shape_bounding_box
+from geometry import calculate_shape_bounding_box, center_shape_on_origin
 
 class mock_design:
     def build_shape(self, params: KeyFobParams): 
@@ -14,13 +14,14 @@ class mock_design:
         textshape = text(text=params.name).linear_extrude(params.height, center=True)
         bounds = calculate_shape_bounding_box(textshape)
         textshape = textshape.translate(*bounds.translation_to_center())
+        #textshape = center_shape_on_origin(textshape)
 
         #nudging the text up so it's not flush with the bottom of the keyfob
         shape -= textshape.up(params.height/2)
 
         #  then translating to the corner for the hole
         shape -= (
-            cylinder(h=params.height * 3, r=1)
+            cylinder(h=params.height * 3, r=2)
             .left(params.length / 2 - distance_from_corner)
             .forward(params.depth / 2 - distance_from_corner)
             .down(params.height)
